ba4c4dfb306c7a174ffe277851aa9c7ac970d8ec
adelinapetkova <89930113+adelinapetkova@users.noreply.github.com>
2021-12-01 10:54:34 +0200

    Add files via upload

---
+++ PythonAdvaced/2_tuples_and_sets/lab/parking_lot.py
@@ -0,0 +1,16 @@
+n=int(input())
+
+parking=set()
+
+for _ in range(n):
+    action, number=input().split(', ')
+    if action=='IN':
+        parking.add(number)
+    elif action=='OUT':
+        parking.remove(number)
+
+if len(parking)==0:
+    print('Parking Lot is Empty')
+else:
+    for num in parking:
+        print(num)
