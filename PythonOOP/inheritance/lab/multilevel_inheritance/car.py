ebb0a7e3097d0e56e1856740709cb762df81b751
adelinapetkova <89930113+adelinapetkova@users.noreply.github.com>
2022-08-23 14:04:14 +0300

    Add files via upload

---
+++ PythonOOP/inheritance/lab/multilevel_inheritance/car.py
@@ -0,0 +1,6 @@
+from inheritance.lab.multilevel_inheritance.vehicle import Vehicle
+
+
+class Car(Vehicle):
+    def drive(self):
+        return "driving..."
---
+++ PythonOOP/inheritance/lab/multilevel_inheritance/sports_car.py
@@ -0,0 +1,8 @@
+from inheritance.lab.multilevel_inheritance.car import Car
+
+
+class SportsCar(Car):
+    def race(self):
+        return "racing..."
+
+
